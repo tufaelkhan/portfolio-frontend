58e848a589ae42c660fa6fdd27a03708c1e89b17
tufaelkhan <tufaelkhan247@gmail.com>
2024-06-14 10:31:25 +0600

    add animation

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-xl">
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/skill'>Skills</Link></li>
         <li><Link to='/project'>Projects</Link></li>
@@ -19,21 +19,21 @@ const NavBar = () => {
         
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-2xl font-semibold font-serif">Tufael Portfolio</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/skill'>Skills</Link></li>
-        <li><Link to='/project'>Projects</Link></li>
-        <li><Link to='/blogs'>Blogs</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/contact'>Contact</Link></li>
-        <li><Link to='/dashboard'>Dashboard</Link></li>
+    <ul className="menu menu-horizontal px-1 text-xl">
+        <li className="hover:text-[#e88b2e] hover:font-semibold"><Link to='/'>Home</Link></li>
+        <li className="hover:text-[#e88b2e] hover:font-semibold"><Link to='/skill'>Skills</Link></li>
+        <li className="hover:text-[#e88b2e] hover:font-semibold"><Link to='/project'>Projects</Link></li>
+        <li className="hover:text-[#e88b2e] hover:font-semibold"><Link to='/blogs'>Blogs</Link></li>
+        <li className="hover:text-[#e88b2e] hover:font-semibold"><Link to='/about'>About</Link></li>
+        <li className="hover:text-[#e88b2e] hover:font-semibold"><Link to='/contact'>Contact</Link></li>
+        <li className="hover:text-[#e88b2e] hover:font-semibold"><Link to='/dashboard'>Dashboard</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <Link to='/contact' className="px-3 py-2 rounded-lg cursor-pointer duration-500 hover:text-white font-semibold bg-[#e88b2e] text-xl">Hire Me</Link>
   </div>
 </div>
   )
--- src/components/ui/home/Contact.jsx
+++ src/components/ui/home/Contact.jsx
@@ -5,12 +5,12 @@ import Container from "../../shared/Container";
 
 const Contact = () => {
   return (
-    <Container bgColor={true}>
+    <Container>
         <div className="">
         <h3 className="text-2xl text-center font-semibold mb-4">My Contact</h3>
         <h1 className="text-4xl text-center font-bold mb-10">I WANT TO HEAR FROM YOU</h1>
         <div className="md:flex sm:w-full">
-            <div className="md:w-2/3">
+            <div className="md:w-2/3" data-aos="fade-right">
                 <input className="text-2xl rounded-lg p-4 w-full m-3 border border-[#e88b2e]" type="text" placeholder="Your Name" onFocus='border border-[#e88b2e]' />
                 <input className="text-2xl rounded-lg p-4 w-full m-3 border border-[#e88b2e]" type="email" placeholder="Your Email" onFocus='border border-[#e88b2e]' />
                 <input className="text-2xl rounded-lg p-4 w-full m-3 border border-[#e88b2e]" type="email" placeholder="Phone Number" onFocus='border border-[#e88b2e]' />
@@ -18,7 +18,7 @@ const Contact = () => {
                 <textarea className="text-2xl rounded-lg p-4 w-full m-3 border border-[#e88b2e]" type="text" placeholder="Message" rows="5"></textarea>
                 <button className="text-2xl rounded-lg px-14 py-2 m-3  bg-[#e88b2e] hover:text-white font-semibold">send message</button>
             </div>
-            <div className="md:ml-20 mt-20">
+            <div className="md:ml-20 mt-20" data-aos="fade-left">
                     <div className="flex items-center mb-7">
                         <FaPhoneAlt className="text-5xl text-[#e88b2e] p-1 rounded-full"/>
                         <p className="text-3xl font-semibold ml-4">+8801601107807</p>
